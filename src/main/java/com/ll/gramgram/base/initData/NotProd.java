24efd00b21bb57523daa18df1b920eff639ad2a7
nagt1997@gmail.com <nagt1997@gmail.com>
2023-04-11 18:04:26 +0900

    modifying naver login

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -27,6 +27,7 @@ public class NotProd {
 
             Member memberUser5ByKakao = memberService.whenSocialLogin("KAKAO", "KAKAO__2736420702").getData();
             Member memberUser6ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__103240209825008011930").getData();
+            Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__{id=t-uKN4aq9pb1ydBG62iF4BuszXmtB7g9ww1J3tQHstM, name=나길태}").getData();
 
             instaMemberService.connect(memberUser2, "insta_user2", "M");
             instaMemberService.connect(memberUser3, "insta_user3", "W");
