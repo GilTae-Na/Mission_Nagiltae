734ac21918cb4a6a166ae7dac5eddd7b1c185588
nagt1997@gmail.com <nagt1997@gmail.com>
2023-05-01 13:14:42 +0900

    set

--- src/main/java/com/ll/gramgram/base/initData/NotProd.java
+++ src/main/java/com/ll/gramgram/base/initData/NotProd.java
@@ -50,7 +50,7 @@ public class NotProd {
                 Member memberUser7ByGoogle = memberService.whenSocialLogin("GOOGLE", "GOOGLE__103240209825008011930").getData();
                 Member memberUser7ByNaver = memberService.whenSocialLogin("NAVER", "NAVER__t-uKN4aq9pb1ydBG62iF4BuszXmtB7g9ww1J3tQHstM").getData();
                 Member memberUser9ByFacebook = memberService.whenSocialLogin("FACEBOOK", "FACEBOOK__4203574466535283").getData();
-
+                //변경예정
                 instaMemberService.connect(memberUser2, "insta_user2", "M");
                 instaMemberService.connect(memberUser3, "insta_user3", "W");
                 instaMemberService.connect(memberUser4, "insta_user4", "M");
